cbf59456e7c492142f2ed97d25a9f730ba1d3749
Igor <ik19890104@gmail.com>
2018-03-27 21:41:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,82 @@
+
+
+import java.util.Scanner;
+
+
+
+
+
+public class Person{
+
+    private String name;
+    private int apples;
+
+public person(String name,int apples){
+    this.name=name;
+    this.apples=apples;
+    }
+public String getName(){
+    return name;
+}
+    public int getApples(){
+    return apples;
+    }
+public int countDifference(Person person){
+        return this.apples-person getApples();
+}
+
+
+
+
+}
+
+public class Run{
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+    private  int age;
+    private  String name;
+
+    private void printInfo() {
+        System.out.println(getAge());
+        System.out.println(getName());
+    }
+
+    public static void main(String[] args) {
+        int age =10;
+        Main dog= new Main();
+        dog.printInfo();
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+*/
+
